a022ed4fe90ab3a3c307fc30416556cefb3c9120
shaju george <shajugeorge45@gmail.com>
2020-10-14 14:20:40 +0530

    advance validation

--- js/button.js
+++ js/button.js
@@ -1,26 +1,45 @@
 function myfun(){
-	var name = document.getElementById("name").value.trim();
-	var num = document.getElementById("num").value;
+	var name = document.getElementById("name");
+	var num = document.getElementById("num");
 	var select = document.getElementById("select");
-	var time = document.getElementById("book_time").value.trim();
+	var time = document.getElementById("book_time");
 
-	if(name == "" || name == null){
+	var regx =/[a-z]/i;
+	var regx1 =/[0-9@!`~#$%^&*(_)+={}[\;.]/;
+
+	if(name.value.trim() == "" || name.value.trim() == null){
 		
-		document.getElementById("namep").innerHTML="Invalid name";
+		document.getElementById("namep").innerHTML="Name cannot be empty";
+		name.style.border= "2px solid red";
 		return false;
 	}
-	else if (num =="" || num == null){
+	else if(regx1.test(name.value)){
+		
+		document.getElementById("namep").innerHTML="Only alphabets";
+		name.style.border= "2px solid red";
+		return false;
+	}
+	else if (num.value =="" || num.value == null){
 		
 		document.getElementById("nump").innerHTML="Invalid phone number";
+		num.style.border= "2px solid red";
 		return false;
 	}
-	else if (num.length != 10){
+	else if (regx.test(num.value)){
 		
-		document.getElementById("nump").innerHTML="phone number is 10 digit";
+		document.getElementById("nump").innerHTML="Only digits allowed";
+		num.style.border= "2px solid red";
 		return false;
 	}
-	else if (time ==""|| time == null){
+	else if (num.value.trim().length != 10){
+		
+		document.getElementById("nump").innerHTML="Number is 10 digit";
+		num.style.border= "2px solid red";
+		return false;
+	}
+	else if (time.value.trim() ==""|| time.value.trim() == null){
 		document.getElementById("timep").innerHTML="Invalid time";
+		time.style.border= "2px solid red";
 		return false;
 	}
 	else {
